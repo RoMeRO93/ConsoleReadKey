790401b33cae764c312d99d9cb34c550159b3952
RoMeRO93 <122970841+RoMeRO93@users.noreply.github.com>
2023-05-04 15:51:50 +0300

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        byte bytex = 255;
+        byte bytey = 1;
+        int xnegative = -2147483647;
+        int xnegative1 = xnegative / 2;
+        uint x = 4294967295;
+        Console.WriteLine(bytex);
+        Console.WriteLine(bytex - bytey);
+        Console.WriteLine(xnegative);
+        Console.WriteLine(xnegative1);
+        Console.WriteLine(x);
+        Console.WriteLine("Salutare lume!");
+        Console.WriteLine("Apasa   o cheie ca sa inchei programul ");
+        Console.ReadKey ();
+    }
+}
